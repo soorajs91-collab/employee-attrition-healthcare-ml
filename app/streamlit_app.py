7670cda3790ade398db1558c6b6a63fd7c4f3872
soorajs91-collab <soorajs91@gmail.com>
2026-05-13 22:56:30 +0530

    Built Streamlit attrition prediction app (updated)

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -46,13 +46,54 @@ overtime_yes = 1 if overtime == "Yes" else 0
 # Prediction button
 if st.button("Predict Attrition"):
 
-    input_data = pd.DataFrame({
-        'Age': [age],
-        'MonthlyIncome': [monthly_income],
-        'YearsAtCompany': [years_at_company],
-        'DistanceFromHome': [distance_from_home],
-        'OverTime_Yes': [overtime_yes]
-    })
+    input_dict = {
+    'Age': age,
+    'DailyRate': 0,
+    'DistanceFromHome': distance_from_home,
+    'Education': 0,
+    'EmployeeCount': 1,
+    'EnvironmentSatisfaction': 0,
+    'HourlyRate': 0,
+    'JobInvolvement': 0,
+    'JobLevel': 0,
+    'JobSatisfaction': 0,
+    'MonthlyIncome': monthly_income,
+    'MonthlyRate': 0,
+    'NumCompaniesWorked': 0,
+    'PercentSalaryHike': 0,
+    'PerformanceRating': 0,
+    'RelationshipSatisfaction': 0,
+    'StandardHours': 80,
+    'Shift': 0,
+    'TotalWorkingYears': 0,
+    'TrainingTimesLastYear': 0,
+    'WorkLifeBalance': 0,
+    'YearsAtCompany': years_at_company,
+    'YearsInCurrentRole': 0,
+    'YearsSinceLastPromotion': 0,
+    'YearsWithCurrManager': 0,
+
+    # Encoded categorical columns
+    'BusinessTravel_Travel_Frequently': 0,
+    'BusinessTravel_Travel_Rarely': 0,
+    'Department_Maternity': 0,
+    'Department_Neurology': 0,
+    'EducationField_Life Sciences': 0,
+    'EducationField_Marketing': 0,
+    'EducationField_Medical': 0,
+    'EducationField_Other': 0,
+    'EducationField_Technical Degree': 0,
+    'Gender_Male': 0,
+    'JobRole_Administrative': 0,
+    'JobRole_Nurse': 0,
+    'JobRole_Other': 0,
+    'JobRole_Therapist': 0,
+    'MaritalStatus_Married': 0,
+    'MaritalStatus_Single': 0,
+    'OverTime_Yes': overtime_yes
+    }
+
+    input_data = pd.DataFrame([input_dict])
 
     prediction = model.predict(input_data)
 
